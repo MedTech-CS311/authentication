124d1e67f54aa86d00e48e7f3b2d1dd3761d545b
Ikdemm <ikdem.benmbarek@aiesec.net>
2022-03-03 15:17:00 +0100

    implemented login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 var express = require("express");
 var router = express.Router();
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Import the User model
 const User = require("../models/user.model");
@@ -13,8 +14,10 @@ const signup = async (req, res) => {
     const userData = req.body;
     // If the email is already in use, you should throw a 409 error
     const user = await User.findOne({ email: userData.email });
+
     if (user) {
       res.status(409).send({ message: "Email Already in use" });
+      return;
     }
 
     // Hash the password with bcrypt
@@ -35,9 +38,27 @@ const signup = async (req, res) => {
 
 const login = (req, res) => {
   // Get the user data from request body
+  const userData = req.body;
   // Throw a 404 if the user does not exist in the list of the users
-  // Compare the password with bcrypt
-  // Throw a 403 error if password is incorrect
+  User.findOne({ email: userData.email }).then((dbUser) => {
+    if (!dbUser) {
+      res.status(404).send({ message: "User not found" });
+    }
+
+    // Compare the password with bcrypt
+    bcrypt
+      .compare(userData.password, dbUser.password)
+      .then(() => {
+        const token = jwt.sign(userData.email, "secret");
+        res.status(200).send({ user: outputUserData, token });
+      })
+      .catch((error) => {
+        console.log(error);
+        // Throw a 403 error if password is incorrect
+        res.status(403).send({ message: "Incorrect password" });
+      });
+  });
+
   // Return an accessToken with jwt
 };
 
